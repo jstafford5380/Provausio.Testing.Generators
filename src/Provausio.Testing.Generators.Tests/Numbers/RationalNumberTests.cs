ed9f40c652cd9c858fe790fd9e3186fb3e5dcf31
Jeremy Stafford <jstafford@digitalairstrike.com>
2019-02-20 18:29:31 -0700

    Added rational number generator and coverage

---
+++ src/Provausio.Testing.Generators.Tests/Numbers/RationalNumberTests.cs
@@ -0,0 +1,56 @@
+﻿using Provausio.Testing.Generators.Generators.Numbers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Provausio.Testing.Generators.Tests.Numbers
+{
+    public class RationalNumberTests
+    {
+        private RationalNumberGenerator _gen;
+
+        public RationalNumberTests()
+        {
+            _gen = new RationalNumberGenerator();
+        }
+
+        [Fact]
+        public void GenerateDouble_GeneratesDouble()
+        {
+            // arrange
+
+            // act
+            var d = _gen.Generate<double>(1, 1000, RationalType.Double);
+
+            // assert
+            Assert.IsType<double>(d);
+            Assert.True(d >= 1 && d <= 1000);
+        }
+
+        [Fact]
+        public void GenerateDecimal_GeneratesDecimal()
+        {
+            // arrange
+
+            // act
+            var d = _gen.Generate<decimal>(1, 1000, RationalType.Decimal);
+
+            // assert
+            Assert.IsType<decimal>(d);
+        }
+
+        [Fact]
+        public void GenerateMoney_OnlyTwoDecimalPlaces()
+        {
+            // arrange
+
+            // act
+            var d = _gen.Generate<decimal>(1, 1000, RationalType.Money);
+
+            // assert
+            var split = d.ToString().Split(",");
+            Assert.True(split[1].Length < 3);
+        }
+    }
+}
---
+++ src/Provausio.Testing.Generators/Generators/Numbers/RationalNumberGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Provausio.Testing.Generators.Generators.Numbers
+{
+    internal class RationalNumberGenerator
+    {
+        private readonly Random _rand = new Random();
+
+        public T Generate<T>(int min, int max, RationalType treatAsType = RationalType.Decimal)
+        {
+            T val;
+            var number = Generate(min, max);
+
+            if (treatAsType == RationalType.Money)
+            {
+                var money = (decimal) Convert.ChangeType(number, typeof(decimal));
+                val = (T) Convert.ChangeType(Math.Round(money, 2), typeof(T));
+            }
+            else val = (T) Convert.ChangeType(number, typeof(T));
+
+            return val;
+        }
+
+        private object Generate(int min = 1, int max = 10000) => _rand.Next(min, max) + _rand.NextDouble();
+    }
+
+    public enum RationalType
+    {
+        Float,
+        Double,
+        Decimal,
+        Money
+    }
+}
